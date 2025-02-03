956390097884b079466d2b71f135932cbc6fc59b
Matias Paez <paezmatias057@gmail.com>
2025-02-03 11:26:18 -0300

    Agregamos la funcionalidad de agregar amigos a la lista, con sus verificaciones.

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
-//  Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-
 function agregarAmigo (){
+    let amigoHTML = document.getElementById("amigo"); //obtengo el elemento
+    let nombre = amigo.value; // obtengo el valor del campo
 
+    if (validarNombre(nombre)){
+        if(!amigos.includes(nombre)){
+            amigos.push(nombre); //agrego el nombre a la lista 'amigos'
+            amigoHTML.value = ''; //limpio el campo
+        }else{
+            alert('El nombre ya esta en la lista');
+            amigoHTML.value = ''; //limpio el campo
+        }
+    }else {
+        alert('Ingrese un nombre válido');
+        amigoHTML.value = ''; //limpio el campo
+    }
+    
 }
 
 function sortearAmigo(){
 
 }
 
-function validarNombre() {
-    
+function validarNombre(nombre) {
+    const expReg = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/; //expresion regular para validar el nombre
+
+    return expReg.test(nombre); //devuelve true - false
 }
 
