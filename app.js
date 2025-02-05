2186b3a8d8a8fca02e2f818e022ce0c099940bd7
Matias Paez <paezmatias057@gmail.com>
2025-02-05 00:23:39 -0300

    Agregamos la funcionalidad mostrar Amigos- visualiza los elementos de la lista amigos en la lista -ul- del HTML - agregando componentes li

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-let amigos = [];
+let amigos = []; // array de amigos
+let ulAmigosHTML = document.getElementById('listaAmigos')
 
 function agregarAmigo (){
     let amigoHTML = document.getElementById("amigo"); //obtengo el elemento
-    let nombre = amigo.value; // obtengo el valor del campo
-
+    let nombre = amigo.value.toUpperCase(); // obtengo el valor del campo
+    
     if (validarNombre(nombre)){
         if(!amigos.includes(nombre)){
             amigos.push(nombre); //agrego el nombre a la lista 'amigos'
-            amigoHTML.value = ''; //limpio el campo
+            limpiarCampo(amigoHTML); //limpio el campo
+            mostrarAmigos();
         }else{
             alert('El nombre ya esta en la lista');
-            amigoHTML.value = ''; //limpio el campo
+            limpiarCampo(amigoHTML); //limpio el campo
         }
     }else {
         alert('Ingrese un nombre válido');
-        amigoHTML.value = ''; //limpio el campo
+        limpiarCampo(amigoHTML); //limpio el campo
     }
     
 }
 
+function limpiarCampo(elemento){
+    elemento.value='';
+}
+
+function mostrarAmigos (){
+    var ulHTML = document.getElementById('listaAmigos');
+    ulHTML.innerHTML = ''; //Limpio la lista - o la establezco como vacia
+
+    if (amigos.length > 0 ){
+        amigos.forEach(element => {
+            alert(element);
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(element));
+            ulHTML.appendChild(li);       
+        });
+    }
+}
+
 function sortearAmigo(){
+    
+        
 
 }
 
